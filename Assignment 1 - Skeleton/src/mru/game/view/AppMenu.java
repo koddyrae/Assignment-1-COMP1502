9f478907275cb59af8176ad1f694d6d19c71c111
Koddy Rae Madriaga <koddyrae@gmail.com>
2023-02-08 10:07:00 -0700

    main menu ui
    started

--- Assignment 1 - Skeleton/src/mru/game/view/AppMenu.java
+++ Assignment 1 - Skeleton/src/mru/game/view/AppMenu.java
@@ -1,5 +1,6 @@
 package mru.game.view;
 
+import java.util.Scanner;
 
 public class AppMenu {
 
@@ -7,4 +8,15 @@ public class AppMenu {
 	 * This class will be used to show the menus and sub menus to the user
 	 * It also prompts the user for the inputs and validates them 
 	 */
+	
+	public void showMainMenu() {
+		System.out.println("Select One of these Options: ");
+		System.out.println("\n\t (P) Play game");
+		System.out.println("\t (S) Search");
+		System.out.println("\t (E) Exit");
+		System.out.println("\nEnter a choice: ");
+		
+		Scanner key = new Scanner(System.in);
+		String choice = key.nextLine();
+	}
 }
