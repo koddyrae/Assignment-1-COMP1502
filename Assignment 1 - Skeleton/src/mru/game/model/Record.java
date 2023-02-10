4343941cbe9fdfc31dbd347e423de1add6fc3363
Koddy Rae Madriaga <koddyrae@gmail.com>
2023-02-10 16:37:21 -0700

    moving find methods to record
    they go here instead

--- Assignment 1 - Skeleton/src/mru/game/model/Record.java
+++ Assignment 1 - Skeleton/src/mru/game/model/Record.java
@@ -85,5 +85,46 @@ public class Record {
         }
 
     }
+    
+	/**
+	 * @author Koddyrae Method to return the player(s) in the arraylist by name
+	 * @param name user inputted search name
+	 * @return name that contains the user input
+	 */
+	public String findPlayersByName(String name) {
+		String result = "";
+
+		for (int i = 0; i < listOfPlayers.size(); i++) {
+			if (listOfPlayers.get(i).getName().toUpperCase().contains(name.toUpperCase())) {
+				result += listOfPlayers.get(i).toString() + ",";
+			}
+		}
+		return result;
+	}
+	
+	/**
+	 * @author Koddyrae Method to return the top player(s) in the arraylist by wins
+	 * @return top player(s) based on order from arraylist
+	 */
+	public String findTopPlayers() {
+		String result = "";
+
+		int max = listOfPlayers.get(0).getWins();
+
+		for (int i = 0; i < listOfPlayers.size(); i++) {
+			if (max < listOfPlayers.get(i).getWins()) {
+				max = listOfPlayers.get(i).getWins();
+			}
+		}
+
+		for (int j = 0; j < listOfPlayers.size(); j++) {
+			if (max == listOfPlayers.get(j).getWins()) {
+				result += listOfPlayers.get(j).getName() + "," + listOfPlayers.get(j).getWins() + ",";
+			}
+		}
+
+		return result;
+
+	}
 
 }
