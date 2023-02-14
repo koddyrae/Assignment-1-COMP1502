c8b97335387c06195f3bcd05554b1b8deb6e039e
Koddy Rae Madriaga <koddyrae@gmail.com>
2023-02-14 14:59:09 -0700

    gamblertest finished
    all junits should be done, now all thats left is documentation and cleaning up the code

--- Assignment 1 - Skeleton/test/mru/game/test/GamblerTest.java
+++ Assignment 1 - Skeleton/test/mru/game/test/GamblerTest.java
@@ -1,12 +1,13 @@
 package mru.game.test;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
+import mru.game.controller.Card;
 import mru.game.controller.Gambler;
 import mru.game.model.Player;
 
@@ -15,14 +16,16 @@ class GamblerTest {
 	Gambler testGambler;
 	Gambler testPlayer;
 	Player player;
+	Card testCard;
 	
 	@BeforeEach
 	public void setUp() {
 		banker = new Gambler(true); // Create a banker.
 		testGambler = new Gambler("Banker");
 		player = new Player("ah", 10, 10);
-		// A non-(financial)-banker gambler named Banker; named after one who banks, as in a pilot banking the plane in a given direction. Ask their parents, not me.
 		testPlayer = new Gambler(player);
+		testCard = new Card(11, "Diamond");
+		testGambler.addCardToHand(testCard);
 	}
 	
 	@Test
@@ -35,4 +38,43 @@ class GamblerTest {
 		assertNotEquals(player.toString(), testPlayer.toString());
 	}
 
+	@Test 
+	void testScoreHand() {
+		assertEquals(0, testGambler.scoreHand());
+	}
+
+	@Test
+	void testAddCardToHand() {
+		assertNotNull(testGambler.getHand());	
+	}
+	
+	@Test
+	void testSetAndGetBalance() {
+		testGambler.setBalance(100);
+		assertEquals(100, testGambler.getBalance());
+	}
+	
+	@Test
+	void testSetAndGetWins() {
+		testGambler.setWins(10);
+		assertEquals(10, testGambler.getWins());
+	}
+	
+	@Test
+	void testSetAndGetScore() {
+		testGambler.setScore(10);
+		assertEquals(10, testGambler.getScore());
+	}
+	
+	@Test 
+	void testSetAndGetAdmitted() {
+		testGambler.setAdmittedToCasino(true);
+		assertTrue(testGambler.getAdmittedToCasino());
+	}
+	
+	@Test
+	void testIncrementWins() {
+		testGambler.incrementNumberOfWins();
+		assertEquals(1, testGambler.getWins());
+	}
 }
