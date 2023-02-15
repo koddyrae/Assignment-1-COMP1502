f87df1d19d8f78af6f36fa8657c77dfe85e9d798
Bryce Carson <bcars268@mtroyal.ca>
2023-02-14 20:27:54 -0700

    Stage all of the JavaDoc after modifying the two git ignores

---
+++ Assignment 1 - Skeleton/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"mru.game.application"},{"l":"mru.game.controller"},{"l":"mru.game.model"},{"l":"mru.game.view"}];updateSearchResults();
